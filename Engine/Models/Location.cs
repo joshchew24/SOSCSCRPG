4f1a32ece31b9fdff2e095431a5f8ec2fcafdf55
Joshua Chew <joshuagchew@gmail.com>
2021-08-10 17:08:10 -0700

    Worked on adding images to locations. Issue with rendering

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class Location
+    public class Location
     {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
     }
 }
